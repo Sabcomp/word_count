bc623385809da7d9b74a1544e3c7384b7c084b30
Sabcomp <alushisabrina93@gmail.com>
2021-03-29 09:05:08 +0000

    Add comments

--- word_count.py
+++ word_count.py
@@ -1,3 +1,5 @@
-text = input("What's on your mind today? ")
-word_list = text.split()
-print("oh nice, you just told me what's on your mind in " + str(len(word_list)) + " words!"
+text = input("What's on your mind today? ") # Takes input from the user
+
+word_list = text.split()  # Creates a list of the words
+
+print("oh nice, you just told me what's on your mind in " + str(len(word_list)) + " words!")  # len() function shows the number of elements in word-list
